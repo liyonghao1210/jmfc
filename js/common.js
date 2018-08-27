0c16f098dd4fdce148f545fe480fe79e3f3d47bd
liyonghao1210 <lyh_md@163.com>
2018-08-27 08:53:18 +0800

    完整版

---
+++ js/common.js
@@ -0,0 +1,18 @@
+/*导入头部和尾部*/
+    $(document).ready(function(){
+       $(".RightNav").load("header.html",function(){
+          var urlstr = location.href;
+            var urlstatus=false;
+            console.log(urlstr)
+            $(".linkNav li a").each(function () {
+                if ((urlstr + '/').indexOf($(this).attr('href')) > -1&&$(this).attr('href')!='') {
+                    $(this).addClass('linkActive'); urlstatus = true;
+                } else {
+                    $(this).removeClass('linkActive');
+                }
+            });
+            if (!urlstatus) {$(".linkNav li a").eq(3).addClass('linkActive'); }
+        });
+    });
+
+    
